f0c553ea7d456b28bab75773747a1b02ad3e95e0
Juan Benjumea <juanbenjumea@gmail.com>
2022-04-12 20:12:52 -0500

    Prueba

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 function Header() {
     return (        
-        <h1 className="text-4xl text-center font-black text-white mb-8">Miblioteca</h1>
+        <h1 className="text-4xl text-center font-black text-white mb-8">-Miblioteca-</h1>
     );
 }
 
